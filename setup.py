253560c5b0aa63c131ca2c6c305edf3fcbb93765
PJaros <jaros.paul@gmail.com>
2018-02-24 13:35:14 +0100

    Commiting empty project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyRobiGame',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='PJaros',
+    author_email='',
+    description=''
+)
